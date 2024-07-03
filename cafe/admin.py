1ff964e22a34f3ea72275a839f178a87e7f87564
Your Name <you@example.com>
2024-07-03 10:25:32 +0300

    Add models to admin panel

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from cafe.models import (
+    Cafe,
+    Cuisine,
+    EstablishmentType,
+    CafeLunchMenu,
+    LunchDish
+)
 
-# Register your models here.
+models = [Cuisine, EstablishmentType, LunchDish]
+for model in models:
+    admin.site.register(model)
+
+
+class LunchDishInline(admin.TabularInline):
+    fk_name = 'menu'
+    model = LunchDish
+    extra = 5
+
+
+@admin.register(CafeLunchMenu)
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ["article"]
+    inlines = [LunchDishInline]
+    list_filter = ('cafe',)
+    search_fields = ("cafe__name", "cafe__id")
+
+
+class LunchMenuInline(admin.TabularInline):
+    fk_name = 'cafe'
+    model = CafeLunchMenu
+    extra = 7
+
+
+@admin.register(Cafe)
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ["article"]
+    inlines = [LunchMenuInline]
