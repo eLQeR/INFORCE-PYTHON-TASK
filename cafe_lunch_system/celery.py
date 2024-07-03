a3352b5e4e45329a1c1720adbb35404afed6e1e7
Your Name <you@example.com>
2024-07-03 13:19:38 +0300

    Connect celery in __init__.py, change project name

--- cafe_lunch_system/celery.py
+++ cafe_lunch_system/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cafe_lunch_system.settings')
 
-app = Celery('backend')
+app = Celery('cafe_lunch_system')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
