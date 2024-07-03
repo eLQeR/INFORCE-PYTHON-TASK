2cef9b9323422c01c5870e8c98a1d336f4ae2075
Your Name <you@example.com>
2024-07-03 10:30:36 +0300

    Create ResultOfVoting and Vote models

--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from cafe.models import Cafe
+
+
+class ResultOfVoting(models.Model):
+    voting_date = models.DateField(auto_now_add=True)
+    result_cafe = models.ForeignKey(to=Cafe, on_delete=models.CASCADE, related_name='results_voting')
+    quantity_of_votes = models.IntegerField(default=0)
+
+
+class Vote(models.Model):
+    cafe = models.ForeignKey(to=Cafe, on_delete=models.CASCADE, related_name='votes')
+    created_at = models.DateField(auto_now_add=True)
+    voter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='votes')
+
+    class Meta:
+        unique_together = (('created_at', 'voter'),)
