00eb1c6b04d7a0018f2677f3300d977d38abd62f
Your Name <you@example.com>
2024-07-03 13:20:30 +0300

    Create select_the_winner_cafe shared task

---
+++ voting/tasks.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+import random
+import datetime
+
+from celery import shared_task
+from django.db.models import Count
+
+from voting.models import Vote, ResultOfVoting
+from cafe.models import Cafe
+
+
+
+@shared_task
+def select_the_winner_cafe():
+    today = datetime.date.today()
+
+    # Select all today's votes and group them by cafe
+
+    votes_to_cafes = (
+        Vote.objects.filter(created_at=today)
+        .values('cafe')
+        .annotate(total_votes=Count('id'))
+        .order_by('-total_votes')
+    )
+    if votes_to_cafes:
+        most_voted_cafe = votes_to_cafes.first()
+        ResultOfVoting.objects.create(
+            result_cafe_id=most_voted_cafe['cafe'],
+            quantity_of_votes=most_voted_cafe['total_votes']
+        )
+    else:
+        random_cafe = random.choice(Cafe.objects.all())
+        ResultOfVoting.objects.create(result_cafe=random_cafe, quantity_of_votes=0)
